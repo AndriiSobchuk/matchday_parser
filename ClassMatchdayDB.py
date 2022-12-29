cc58d844b9bed997023b620c86a16e31545e413f
AndriiSobchuk <anri.sobchuk@gmail.com>
2022-12-29 21:35:32 +0200

    Matchday parser for MySQL

--- ClassMatchdayDB.py
+++ ClassMatchdayDB.py
@@ -1,14 +1,14 @@
 from ClassMatchdayParser import LeaguesMatchday, dict_leagues_scores
-import sqlite3
+import mysql.connector
+from DB_connector import db_connect
 
 class MatchdayDB:
 
     #initialize and create table if no exists
-    def __init__(self, DB_name):
-        self.DB_name = sqlite3.connect(DB_name)
-        self.cursor = self.DB_name.cursor()
-        self.cursor.execute('CREATE TABLE IF NOT EXISTS matchday(country text, league text, wk text, m_day text, m_date text, m_time text, m_home text, m_score text, m_away text, unique (m_date, m_home));')
-        #or add here try / catch ?
+    def __init__(self):
+        cursor = db_connect.cursor()
+        cursor.execute("USE DB")
+        cursor.execute('CREATE TABLE IF NOT EXISTS matchday(country text, league text, wk text, m_day text, m_date text, m_time text, m_home text, m_score text, m_away text, unique (m_date(255), m_home(255)));')
 
     #get output from Leagues matchday
     def printData(self):
@@ -18,30 +18,33 @@ class MatchdayDB:
 
     #insert method
     def insertData(self):
+        cursor = db_connect.cursor()
+        cursor.execute("USE DB")
         callParserClass = LeaguesMatchday(dict_leagues=dict_leagues_scores)
         callDataFromValueURL = callParserClass.getDataFromValueURL()
         for country in callDataFromValueURL:
             for game in country:
-                query_string = 'INSERT OR IGNORE INTO matchday VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?);'
-                self.cursor.execute(query_string, game)
-        self.DB_name.commit()
-        self.DB_name.close()
+                query_string = 'INSERT IGNORE INTO matchday VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s);'
+                cursor.execute(query_string, game)
+        db_connect.commit()
+        db_connect.close()
 
     # select method
     def get_data_from_db(self, data_value):
-        self.DB_name = sqlite3.connect('andr.db')
-        self.cursor = self.DB_name.cursor()
-        select_query = "SELECT wk, m_day, m_time, m_home, m_score, m_away FROM matchday WHERE m_date =(?)"
-        self.cursor.execute(select_query, [data_value])
-        for cell in self.cursor.fetchall():
+        db_connect.reconnect()
+        cursor = db_connect.cursor()
+        cursor.execute("USE DB")
+        select_query = "SELECT wk, m_day, m_time, m_home, m_score, m_away FROM matchday WHERE m_date =%s"
+        cursor.execute(select_query, [data_value])
+        for cell in cursor.fetchall():
             print(*cell)
-        self.DB_name.commit()
-        self.DB_name.close()
+        db_connect.commit()
+        db_connect.close()
 
     # update method
 
 
-awd = MatchdayDB('andr.db')
+awd = MatchdayDB()
 awd.insertData()
 #awd = MatchdayDB('andr.db')
 awd.get_data_from_db('2022-12-29')
--- DB_connector.py
+++ DB_connector.py
@@ -1,8 +1,13 @@
 import mysql.connector
 
+# db_connect = mysql.connector.connect(
+#     host="mysql",
+#     user="root",
+#     password="password"
+# )
 db_connect = mysql.connector.connect(
-    host="localhost",
+    host="172.17.0.2",
     user="root",
-    password="password"
+    password="password",
+    port=3306
 )
-
